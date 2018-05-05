e8d2ea350252bcf3841fd0bdbbb74e78281d3157
nicenick <worldsky86rough@gmail.com>
2018-05-05 16:04:29 +0800

    stub ledger

--- app/api.rb
+++ app/api.rb
@@ -9,7 +9,15 @@ module ExpenseTracker
     end
 
     post '/expenses' do
-      JSON.generate 'expense_id' => 42
+      expense = JSON.parse(request.body.read)
+      result = @ledger.record(expense)
+      JSON.generate('expense_id' => result.expense_id)
+    end
+    # trailing colon indicates a required parameter, if missing will raise error
+    # or we can provide a default value after the trailing colon
+    def initialize(ledger: Ledger.new)
+      @ledger = ledger
+      super()
     end
   end
 end
